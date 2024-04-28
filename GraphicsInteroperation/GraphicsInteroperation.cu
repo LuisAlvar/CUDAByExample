4b947855ce4e67781c811c947113824ef2349575
Luis Alvarez <alvarez.en.luis@gmail.com>
2024-04-28 13:24:18 -0700

    done with the main function

--- GraphicsInteroperation/GraphicsInteroperation.cu
+++ GraphicsInteroperation/GraphicsInteroperation.cu
@@ -7,9 +7,18 @@
 #define GL_GLEXT_PROTOTYPES
 #define DIM 512 
 
+PFNGLGENBUFFERSARBPROC    glGenBuffers     = NULL;
+PFNGLBINDBUFFERARBPROC    glBindBuffer     = NULL;
+PFNGLBUFFERDATAARBPROC    glBufferData     = NULL;
+PFNGLDELETEBUFFERSARBPROC glDeleteBuffers  = NULL;
+
 // Need two variables that will store different handles to the same buffer
 GLuint bufferObj; // will be OpenGL's name for the data
 cudaGraphicsResource* resource; //will be the CUDA C name for it 
+
+__global__ void kernel(uchar4* ptr);
+static void draw_func(void);
+static void key_func(unsigned char key, int x, int y);
 
 int main(int argc, char** argv)
 {
@@ -30,8 +39,32 @@ int main(int argc, char** argv)
   glutInitWindowSize(DIM, DIM);
   glutCreateWindow("bitmap");
 
+  glGenBuffers    = (PFNGLGENBUFFERSARBPROC)GET_PROC_ADDRESS("glGenBuffers");
+  glBindBuffer    = (PFNGLBINDBUFFERARBPROC)GET_PROC_ADDRESS("glBindBuffer");
+  glBufferData    = (PFNGLBUFFERDATAARBPROC)GET_PROC_ADDRESS("glBufferData");
+  glDeleteBuffers = (PFNGLDELETEBUFFERSARBPROC)GET_PROC_ADDRESS("glDeleteBuffers");
+
   glGenBuffers(1, &bufferObj);
   glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, bufferObj);
   glBufferData(GL_PIXEL_UNPACK_BUFFER_ARB, DIM * DIM * 4, NULL, GL_DYNAMIC_DRAW_ARB);
+
+  HANDLE_ERROR(cudaGraphicsGLRegisterBuffer(&resource, bufferObj, cudaGraphicsMapFlagsNone));
+
+  uchar4* devPtr;
+  size_t size;
+  HANDLE_ERROR(cudaGraphicsMapResources(1, &resource, NULL));
+  HANDLE_ERROR(cudaGraphicsResourceGetMappedPointer((void**)&devPtr, &size, resource));
+
+  dim3 grids(DIM/16, DIM/16);
+  dim3 threads(16,16);
+
+  kernel<<<grids, threads>>>(devPtr);
+
+  HANDLE_ERROR(cudaGraphicsUnmapResources(1, &resource, NULL));
+
+  // set up GLUT and kick off main loop
+  glutKeyboardFunc(key_func);
+  glutDisplayFunc(draw_func);
+  glutMainLoop();
 
 }
